5831b12f0426cf412d84b6f78668677095620c26
Rohith <r12.rohith@gmail.com>
2018-06-18 12:28:21 -0400

    // Timer setup after clocking in

--- TimeTracker WatchKit Extension/InterfaceController.swift
+++ TimeTracker WatchKit Extension/InterfaceController.swift
@@ -11,7 +11,7 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
+    
     @IBOutlet var topLbl: WKInterfaceLabel!
     @IBOutlet var middleLbl: WKInterfaceLabel!
     @IBOutlet var Btn: WKInterfaceButton!
@@ -31,35 +31,48 @@ class InterfaceController: WKInterfaceController {
     @IBAction func clockInAndClockOut() {
         
         if clockedIn{
-          clockOut()
+            clockOut()
         }else{
-          clockIn()
+            clockIn()
         }
         updateUI(clockedIn: clockedIn)
-
+        
     }
     
     func updateUI(clockedIn: Bool){
         if clockedIn{
-          //UI for clocked in
+            //UI for clocked in
             topLbl.setHidden(false)
             Btn.setTitle("Clocked-Out")
             Btn.setBackgroundColor(UIColor.red)
             middleLbl.setText("5m 44s")
         }else{
-          //UI for clocked out
-          topLbl.setHidden(true)
-          Btn.setTitle("Clocked-In")
-          Btn.setBackgroundColor(UIColor.green)
-          middleLbl.setText("Today\n 3h 44m")
+            //UI for clocked out
+            topLbl.setHidden(true)
+            Btn.setTitle("Clocked-In")
+            Btn.setBackgroundColor(UIColor.green)
+            middleLbl.setText("Today\n 3h 44m")
         }
     }
     
-   
+    
     func clockIn(){
         clockedIn = true
         UserDefaults.standard.set(Date(), forKey: "clockedIn")
         UserDefaults.standard.synchronize()
+        
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
+            if let clockedInDate = UserDefaults.standard.value(forKey: "clockedIn") as? Date{
+               let timeInterval = Int(Date().timeIntervalSince(clockedInDate))
+                
+               let hrs = timeInterval / 3600
+               let mins = (timeInterval % 3600) / 60
+               let secs = timeInterval % 60
+               
+                self.middleLbl.setText("\(hrs)h \(mins)m \(secs)s")
+                
+            }
+        }
     }
     
     func clockOut(){
@@ -86,12 +99,12 @@ class InterfaceController: WKInterfaceController {
             
             //To check if user lost power to watch or loses network after clocking in
             UserDefaults.standard.set(nil, forKey: "clockedIn")
-
-
+            
+            
         }
         
         UserDefaults.standard.synchronize()
-
+        
     }
     
     
