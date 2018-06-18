cd7c5c28c2daa810d2bc002fa7c5dfc1a069ee7e
Rohith <r12.rohith@gmail.com>
2018-06-18 11:54:25 -0400

    // Initial UI Setup

--- TimeTracker WatchKit Extension/InterfaceController.swift
+++ TimeTracker WatchKit Extension/InterfaceController.swift
@@ -12,20 +12,47 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet var topLbl: WKInterfaceLabel!
+    @IBOutlet var middleLbl: WKInterfaceLabel!
+    @IBOutlet var Btn: WKInterfaceButton!
+    var clockedIn: Bool = false
+    
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
         // Configure interface objects here.
+        updateUI(clockedIn: clockedIn)
     }
     
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-    }
     
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
-    }
+    
+    //MARK: Actions
+    
+    @IBAction func clockInAndClockOut() {
+        
+        if clockedIn{
+          clockedIn = false
+        }else{
+          clockedIn = true
+        }
+        updateUI(clockedIn: clockedIn)
 
+    }
+    
+    func updateUI(clockedIn: Bool){
+        if clockedIn{
+          //UI for clocked in
+            topLbl.setHidden(false)
+            Btn.setTitle("Clocked-Out")
+            Btn.setBackgroundColor(UIColor.red)
+            middleLbl.setText("5m 44s")
+        }else{
+          //UI for clocked out
+          topLbl.setHidden(true)
+          Btn.setTitle("Clocked-In")
+          Btn.setBackgroundColor(UIColor.green)
+          middleLbl.setText("Today\n 3h 44m")
+        }
+    }
+    
 }
